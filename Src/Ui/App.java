6a7740f43698bb23709411fb44be1ccc424af758
RAMZ555 <sriramsubramaniyan555@gmail.com>
2025-02-19 16:56:56 +0530

    Create App.java

---
+++ Src/Ui/App.java
@@ -0,0 +1,21 @@
+package ui;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class App extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("login.fxml"));
+        primaryStage.setScene(new Scene(loader.load(), 600, 400));
+        primaryStage.setTitle("Personal Expense Manager");
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
