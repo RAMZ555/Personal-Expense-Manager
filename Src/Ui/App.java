6806a284f3a641ce58d4ce8ff0983f53fd8364e1
RAMZ555 <sriramsubramaniyan555@gmail.com>
2025-02-19 17:20:22 +0530

    Update App.java

--- Src/Ui/App.java
+++ Src/Ui/App.java
@@ -5,12 +5,12 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class App extends Application {
-
+public class MainApp extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("login.fxml"));
-        primaryStage.setScene(new Scene(loader.load(), 600, 400));
+        Scene scene = new Scene(loader.load(), 600, 400);
+        primaryStage.setScene(scene);
         primaryStage.setTitle("Personal Expense Manager");
         primaryStage.show();
     }
